6f010f62b4fa2ec11e940cda21287628b19a6920
Anant Kanchan <anant4265@gmail.com>
2020-06-12 14:12:24 +0530

    initial commit message

---
+++ app/src/main/java/com/anant/media/SongInfo.kt
@@ -0,0 +1,15 @@
+package com.anant.media
+
+class SongInfo{
+
+    var Title:String?=null
+    var AuthorName:String?=null
+    var SongURL:String?=null
+    constructor(Title:String,AuthorName:String,SongURL:String){
+        this.Title=Title
+        this.AuthorName=AuthorName
+        this.SongURL=SongURL
+    }
+
+}
+
